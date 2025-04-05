f3d5d8a8fd91e80825041d24cbbb10f2a8be74f2
Dima Shubitidze <einherjar73@users.noreply.github.com>
2025-04-06 00:13:29 +0400

    made website double language made website double language on georgian and on english made few changes on positions also

--- javascript.js
+++ javascript.js
@@ -1,77 +1,107 @@
+const btns = document.querySelectorAll(".btn");
+const scoreBoard = document.querySelector("#scoreboard");
+const result = document.querySelector("#result");
+const reset = document.querySelector(".reset");
+
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
+Array.from(btns, (btn) => {
+  btn.addEventListener("click", (e) => {
+    let human = e.currentTarget.dataset.id;
+    let computer = getComputerChoice();
+
+    playRound(human, computer);
+  });
+});
+
+function getComputerChoice() {
   let answer = Math.floor(Math.random() * 3);
-  if (answer === 0){
+  if (answer === 0) {
     return "rock";
-  } else if (answer === 1){
+  } else if (answer === 1) {
     return "paper";
   } else {
     return "scissors";
   }
-} 
-
-function getHumanChoice(){
-  let humanChoice = prompt("Enter your answer", "");
-  return humanChoice;
 }
 
-function getScoreBoard(){
-  console.log("ScoreBoard: " + humanScore + "-" + computerScore);
-}
-
-function playRound(humanChoice, computerChoice){
-  if (humanChoice === "rock" && computerChoice === "paper"){
-    computerScore ++;
-    console.log("You lose! Paper beats Rock");
-    getScoreBoard();
-    playGame();
-  } else if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore++;
-    console.log("You lose! Rock beats Scissors");
-    getScoreBoard();
-    playGame();
-  } else if (humanChoice === "paper" && computerChoice === "scissors"){
-    computerScore++;
-    console.log("You lose! Scissors beats Paper");
-    getScoreBoard();
-    playGame();
-  } else if (humanChoice === "paper" && computerChoice === "rock"){
-    humanScore++;
-    console.log("You Win! Paper beats Rock");
-    getScoreBoard();
-    playGame();
-  } else if (humanChoice === "rock" && computerChoice === "scissors"){
-    humanScore++;
-    console.log("You Win! Rock beats Scissors");
-    getScoreBoard();
-    playGame();
-  } else if (humanChoice === "scissors" && computerChoice === "paper"){
-    humanScore++;
-    console.log("You Win! Scissors beats Paper");
-    getScoreBoard();
-    playGame();
-  } else if(humanChoice === computerChoice){
-    console.log("Try again");
-    playGame();
-  }else {
-    console.log("incorrect value");
-  }   
-}
-
-function playGame(){
-  let human;
-  let computer;
-  if(computerScore !== 3 && humanScore !== 3){  //(a || b) == (!a && !b)
-    human = getHumanChoice().toLowerCase();
-    computer = getComputerChoice();
-    playRound(human,computer);
-  }else{
-    if (humanScore > computerScore){
-      console.log("You win " + humanScore + " - " + computerScore);
-    } else {
-      console.log("You lose " + humanScore + " - " + computerScore);
-    }
+function getScoreBoard() {
+  if (document.documentElement.lang == "ka") {
+    scoreBoard.textContent = `შენ ${humanScore} - ${computerScore} კომპიუტერი`;
+  } else {
+    scoreBoard.textContent = `Score: You ${humanScore} - ${computerScore} Computer`;
   }
 }
+
+function playRound(humanChoice, computerChoice) {
+  let computerChoiceGeo;
+  let humanChoiceGeo;
+
+  if (computerChoice === "rock") {
+    computerChoiceGeo = "ქვის";
+  } else if (computerChoice === "paper") {
+    computerChoiceGeo = "ქაღალდის";
+  } else if (computerChoice === "scissors") {
+    computerChoiceGeo = "მაკრატლის";
+  }
+
+  // if (humanChoice === 'rock') {
+  //   humanChoiceGeo = 'ქვის';
+  // } else if (humanChoice === 'paper') {
+  //   humanChoiceGeo = 'ქაღალდის';
+  // } else if (humanChoice === 'scissors') {
+  //   humanChoiceGeo = 'მაკრატლის';
+  // }
+
+  if (
+    (humanChoice === "rock" && computerChoice === "paper") ||
+    (humanChoice === "scissors" && computerChoice === "rock") ||
+    (humanChoice === "paper" && computerChoice === "scissors")
+  ) {
+    computerScore++;
+    if (document.documentElement.lang == "ka") {
+      result.textContent = `წააგე ${computerChoiceGeo} წინააღმდეგ`;
+      result.style.color = "red";
+    } else {
+      result.textContent = `Lose against - ${computerChoice}`;
+      result.style.color = "red";
+    }
+    getScoreBoard();
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    humanScore++;
+    if (document.documentElement.lang == "ka") {
+      result.textContent = `მოიგე ${computerChoiceGeo} წინააღმდეგ`;
+      result.style.color = "#0f0";
+    } else {
+      result.textContent = `Win against - ${computerChoice}`;
+      result.style.color = "#0f0";
+    }
+
+    
+    getScoreBoard();
+  } else if (humanChoice === computerChoice) {
+    if (document.documentElement.lang == "ka"){
+      result.textContent = 'ფრე';
+    } else {
+     result.textContent = `Tie`;
+    }
+    result.style.color = "black";
+  } else {
+    result.textContent = "incorrect value";
+  }
+}
+
+reset.addEventListener("click", () => {
+  if (document.documentElement.lang == "ka"){
+    scoreBoard.textContent = 'შენ 0 - 0 კომპიუტერი'
+  } else {
+    scoreBoard.textContent = "You 0 - 0 Computer";
+  }
+  result.textContent = "...";
+  result.style.color = "black";
+});
